507998ce07653fc1ba29f33336906c6f82bc0325
salim499 <55392107+salim499@users.noreply.github.com>
2020-08-26 04:37:51 +0200

    Update Equipe.js

--- src/routes/Dr_Nicolas_Zwillinger/Equipe.js
+++ src/routes/Dr_Nicolas_Zwillinger/Equipe.js
@@ -7,27 +7,8 @@ import * as ReactBootStrap from 'react-bootstrap';
 function Equipe(){
     return(        
       <FooterContainer>
-       /* <div className="" style={{msAlignSelf:"center"}} >
-          <ReactBootStrap.CardGroup horizontal>
-           <CardPerson src1="./docteur-nicolas-zwillinger-10-615x410.jpg" src2="./Dr_Zwillinger.jpeg"
-            src3="./102963448_3175530212499376_5471271786858869558_o.jpg" title="DR NICOLAS ZWILLINGER" 
-            bio="CHIRURGIEN PLASTICIEN, c’est l’intervenant principal de votre opération"
-              link="/Docteur_Zwillinger">            
-            </CardPerson>
-           <CardPerson src1="./sevrine.jpg" src2="./sevrine.jpg" src3="./sevrine.jpg" title="SÉVERINE" 
-          bio="L’ASSISTANTE MÉDICO-ADMINISTRATIVE, elle s’assurera du bon déroulement de votre séjour à la clinique"
-           link="/Severine">
-           </CardPerson>
-           <CardPerson src1="./roxana.jpg" src2="./roxana2.jpg" src3="./roxana3.jpg" title="Dr Roxana Aldea" 
-           bio="Elle est Médecin anesthésiste-réanimateur" link="/Docteur_Aldea">
-
-           </CardPerson>
-           <CardPerson src1="./caraco.jpeg" src2="./caraco3.jpg" src3="./caraco2.jpeg" title="Dr Jean-Jacques Caraco" 
-           bio="Il est Médecin anesthésiste-réanimateur" link="/Docteur_Caraco">
-           </CardPerson>
-          </ReactBootStrap.CardGroup>
-          </div>*/
-		  equipe
+   
+          <h1>equipe</h1>
         </FooterContainer>
     )
 }
